9b2d16e89cc7333228cea1000565d8833f9d0aa1
niraj-karde <niraj.karde@logituit.com>
2024-08-12 23:30:36 +0530

    basic app layout complete

---
+++ hianime-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hianime-clone
+//
+//  Created by apple on 12/08/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ hianime-clone/hianime_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  hianime_cloneApp.swift
+//  hianime-clone
+//
+//  Created by apple on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct hianime_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
